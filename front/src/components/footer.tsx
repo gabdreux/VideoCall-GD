53a4f5e60c4c04e0580806faa01f76a325236827
gabriel dreux <dreuxgabriel@hotmail.com>
2023-08-08 21:31:32 -0300

    login layout

--- front/src/components/footer.tsx
+++ front/src/components/footer.tsx
@@ -15,13 +15,13 @@ const Footer: React.FC = () => {
         <p className="col-md-4 mb-0 text-body-secondary">© 2023 Company, Inc</p>
 
         <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none navbar-brand">
-            <img src='imgs/logo2.png'/>
+            <img src='imgs/logo1.png'/>
         </a>
 
         <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
             <li className="ms-3"><a className="text-body-secondary" href="#"><i className='fab fa-whatsapp'><use href="#"></use></i></a></li>
-            <li className="ms-3"><a className="text-body-secondary" href="#"><i className='fab fa-whatsapp'><use href="#"></use></i></a></li>
-            <li className="ms-3"><a className="text-body-secondary" href="#"><i className='fab fa-whatsapp'><use href="#"></use></i></a></li>
+            <li className="ms-3"><a className="text-body-secondary" href="#"><i className='fab fa-instagram'><use href="#"></use></i></a></li>
+            <li className="ms-3"><a className="text-body-secondary" href="#"><i className='fab fa-facebook'><use href="#"></use></i></a></li>
         </ul>
 
   </footer>
--- front/src/pages/login.tsx
+++ front/src/pages/login.tsx
@@ -2,8 +2,46 @@ import React from 'react';
 
 const Login: React.FC = () => {
   return (
-    <div>
-      <h1>Welcome to the Login Page</h1>
+
+    < div className="containerDiv">
+      <form className="loginForm" data-bitwarden-watching="1">
+
+          
+          <div className='containerDiv'>
+            <a href="/" className="navbar-brand">
+              <img className='loginLogo' src='imgs/logo4.png'/>
+            </a>
+          </div>
+
+          <div className='containerDiv'>
+            <h1 className="h3 mb-3 fw-normal">Login</h1>
+          </div>
+
+          <div className="form-floating">
+            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+            <label htmlFor="floatingInput">Email address</label>
+          </div>
+
+
+          <div className="form-floating">
+            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+            <label htmlFor="floatingPassword">Password</label>
+          </div>
+
+
+          <div className="form-check text-start my-3">
+            <input className="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault" />
+            <label className="form-check-label" htmlFor="flexCheckDefault">
+              Remember me
+            </label>
+          </div>
+
+
+          <button className="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+
+          <p className="mt-5 mb-3 text-body-secondary">© 2017–2023</p>
+
+      </form>
     </div>
   );
 };
